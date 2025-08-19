947172a66d7fb5921a8645c5a39332770d1763b2
canaya2002 <canaya917@gmail.com>
2025-08-19 01:03:15 -0600

    Guardando cambios antes de limpiar el historial

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://www.humanis.com.mx/", 
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-  outDir: "./public",             
-};
