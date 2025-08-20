9c5628b672865ec0acdbf58c2778d4503db01abb
canaya2002 <canaya917@gmail.com>
2025-08-20 17:36:22 -0600

    Arreglos semi

--- .next/server/app/_not-found/page.js
+++ .next/server/app/_not-found/page.js
@@ -3,7 +3,7 @@ const runtime = require("../../chunks/ssr/[turbopack]_runtime.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_618b3959._.js");
 runtime.loadChunk("server/chunks/ssr/[root-of-the-server]__d8593af1._.js");
 runtime.loadChunk("server/chunks/ssr/app_1f3630ef._.js");
-runtime.loadChunk("server/chunks/ssr/[root-of-the-server]__c9e11564._.js");
+runtime.loadChunk("server/chunks/ssr/[root-of-the-server]__b145bb25._.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_next_dist_client_components_1c8c21fa._.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_next_dist_client_components_builtin_forbidden_651c1c6d.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_next_dist_1fff96f2._.js");
